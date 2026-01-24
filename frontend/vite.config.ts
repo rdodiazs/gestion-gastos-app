531208c9819ac3cf72703f180388a73396ff5f5e
Ricardo Díaz Sarmiento <rdo.diaz.s@gmail.com>
2026-01-24 18:27:28 -0300

    feature: config inicial docker frontend
    Se agrega la configuracion inicial para trabajar en Docker la version
    React con Typescript del frontend de esta aplicacion. Dado que estoy
    trabajando en Windows con WSL2, tuve que modificar el archivo
    'vite.config.ts' para poder trabajar con Vite. Explico eso con un poco
    mas de detalle en los comentarios de esas lineas de codigo agregadas.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,21 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Este subobject 'server' debe agregarse para que el servidor
+  // de Vite funcione desde Docker hacia nuestro navegador
+  // Para mas info, revisar:
+  // - https://dev.to/ysmnikhil/how-to-build-with-react-or-vue-with-vite-and-docker-1a3l
+  // - https://medium.com/@tomas.ew88/recently-i-was-setting-up-docker-to-work-with-a-vite-project-and-also-an-express-server-ab65456e64d5
+  // - https://vite.dev/config/server-options
+  server: {
+      host: true,
+      port: 5173,
+      // La opcion 'watch' definido con 'usePolling: true' es para que los cambios en los
+      // archivos se muestren en el navegador al usar Docker en Windows (con WSL2).
+      // Se debe tener en cuenta que esta opcion sobreexige un poco la CPU. Otra opcion es llevar
+      // el proyecto a una carpeta de WSL2 para no tener que usar esta opcion.
+      watch: {
+          usePolling: true
+      },
+  },
 })
